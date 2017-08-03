e4d1133ed18d8821007040260245b8ce49259551
Nicolas <mataratas04@hotmail.com>
2017-08-04 01:56:33 +0200

    Tonic.py and randomgeneration.py

--- util/randomgeneration.py
+++ util/randomgeneration.py
@@ -1,6 +1,9 @@
 import secrets
+import discord
+from discord.ext import commands
 
-async def tonic_token(bot):
-     generatedToken = secrets.token_urlsafe(32);
-
-     return generatedToken
+@commands.command(pass_context=True)
+async def tonictoken(ctx):
+    """Generate random Tonic Token"""
+     generatedToken = secrets.token_urlsafe(32)
+     await ctx.bot.send_message(ctx.message.channel, generatedToken)
