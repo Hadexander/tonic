9027d647ab3a2500d5e67c433d089430e159aef8
Nicolas <mataratas04@hotmail.com>
2017-08-04 02:48:12 +0200

    Fixed tonictoken command

--- util/__init__.py
+++ util/__init__.py
@@ -2,3 +2,4 @@ from .prefix import serverwide_prefix, command_prefix
 from util.maintenance import access, avatar
 from util.prefix import prefix
 from util.checks import VerificationError
+from util.randomgeneration import tonictoken
--- util/randomgeneration.py
+++ util/randomgeneration.py
@@ -4,6 +4,6 @@ from discord.ext import commands
 
 @commands.command(pass_context=True)
 async def tonictoken(ctx):
-    """Generate random Tonic Token"""
+     """Generate random Tonic Token"""
      generatedToken = secrets.token_urlsafe(32)
      await ctx.bot.send_message(ctx.message.channel, generatedToken)
