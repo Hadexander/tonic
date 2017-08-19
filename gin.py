089a9d1841d40b2a3953c38fdc8bb5fd78266e06
SchrodingersKitty <radium213@gmail.com>
2017-08-20 02:20:04 +0300

    Using shell correctly now.

--- gin.py
+++ gin.py
@@ -3,20 +3,22 @@ import os
 from time import sleep
 
 print('Gin is now serving Tonic.')
-cmd = ['python',  'tonic.py']
-gitcmd=['git', 'pull']
+cmd = 'python tonic.py'
+gitcmd='git pull'
 
 def gitPull():  
     print('Pulling from git')
     subprocess.run(gitcmd, shell=True)
 
 def serveTonic():
-    return subprocess.run(cmd, shell=True).returncode
+    return subprocess.run(cmd, shell=True)
 
 exitCode = -1
 while(exitCode != 0):
-    exitCode = serveTonic()
+    result = serveTonic()
+    exitCode = result.returncode
     print('Tonic exited with code {}'.format(exitCode))
+    print(result.stdout)
     sleep(5)
     if exitCode == 420:
         gitPull()
