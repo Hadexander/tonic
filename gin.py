39effe534eea53d151bd26afd977fdbda85e760b
Nicolas <mataratas04@hotmail.com>
2017-08-19 20:19:02 +0200

    Ubuntu

--- gin.py
+++ gin.py
@@ -2,20 +2,20 @@ import subprocess
 import os
 
 print('Gin is now serving Tonic.')
-cmd = 'python tonic.py'
-gitcmd = 'git pull'
+cmd = ['python',  'tonic.py']
+gitcmd=['git', 'pull']
 
-def gitPull():
+def gitPull():  
     print('Pulling from git')
     subprocess.run(gitcmd)
 
 def serveTonic():
-    run = subprocess.run(cmd)
+    run = subprocess.run('cmd')
     exitCode = run.returncode
     if exitCode == 420:
         print('Client close detected')
         print(exitCode)
         gitPull()
-        serveTonic()
+        #serveTonic()
 
 serveTonic()
