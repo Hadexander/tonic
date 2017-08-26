758f225d430642b410fc9d6c8397e74c561630c6
SchrodingersKitty <radium213@gmail.com>
2017-08-26 18:47:03 +0300

    Actual search functionality for poewiki

--- poe/wiki.py
+++ poe/wiki.py
@@ -4,29 +4,65 @@ import discord
 from discord.ext import commands
 from lxml import html
 
-def get_printouts(json, item):
-    return json['query']['results'][item]['printouts']
+wikiurl = 'https://pathofexile.gamepedia.com'
+search_limit = 5
 
-def get_imageurl(json):
-    return json['Has inventory icon'][0]['fullurl'].replace('File:','Special:Filepath/')
+def _urlencode(wiki, **kwargs):
+    kwargs['format'] = 'json'
+    return wiki + '/api.php?' + urllib.parse.urlencode(kwargs, quote_via=urllib.parse.quote)
 
-def get_infobox(json):
-    return json['Has infobox HTML'][0]
+def _jsonpath(json, path):
+    i = 0
+    while json and i < len(path):
+        json = json.get(path[i], [])
+        i += 1
+    return json
 
-@commands.command(pass_context=True)
-async def poewiki(ctx, *args):
-    """Looks up an item on the Path of Exile wiki. Case sensitive. No warranty."""
-    item = ' '.join(args)
-    url = 'https://pathofexile.gamepedia.com/api.php?action=askargs&format=json&conditions=Has%20name::{}&printouts=Has%20infobox%20HTML%7CHas%20inventory%20icon'\
-    .format(urllib.parse.quote(item))
+def _smwencode(*args):
+    return '|'.join(args)
+
+async def wiki_get_item(session, wiki, name):
+    url = _urlencode(wiki, action='askargs', conditions=_smwencode('Has name::'+name), printouts=_smwencode('Has tags', 'Has infobox HTML', 'Has inventory icon'))
+    async with session.get(url) as response:
+        if(response.status != 200):
+            return None
+        json = await response.json()
+    json = _jsonpath(json, ['query', 'results', name, 'printouts'])
+    if not json:
+        return None
+    item = dict()
+    item['name'] = name
+    item['tags'] = json.get('Has tags')
+    item['infobox'] = json.get('Has infobox HTML')[0]
+    icon = json.get('Has inventory icon')[0]
+    item['image'] = icon.get('fullurl', '').replace('File:','Special:Filepath/')
+    return item
+
+async def wiki_search(wiki, text, limit):
+    url = _urlencode(wiki, action='query', list='search', srsearch=text, srinfo='', srprop='', srlimit=limit)
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             if(response.status != 200):
-                return
+                return []
             json = await response.json()
-    printouts = get_printouts(json, item)
-    imgurl = get_imageurl(printouts)
-    infobox = get_infobox(printouts)
+        json = _jsonpath(json, ['query', 'search'])
+        results = [r.get('title') for r in json]
+        items = []
+        for name in results:
+            item = await wiki_get_item(session, wiki, name)
+            if item:
+                items.append(item)
+    return items
+
+@commands.command(pass_context=True)
+async def poewiki(ctx, *args):
+    """Looks up an item on the Path of Exile wiki."""
+    itemname = ' '.join(args)
+    matches = wiki_search(wikiurl, itemname, search_limit)
+    if not matches:
+        await ctx.bot.send_message(ctx.message.channel, 'No item matches "{}"'.format(itemname))
+    infobox = matches[0]['infobox']
+    image = matches[0]['image']
     tree = html.fromstring(infobox)
     header = tree.xpath("//span[contains(@class, 'header')]//text()")
     desc = '\n'.join(header[1:])+'\n'
@@ -40,5 +76,8 @@ async def poewiki(ctx, *args):
     desc  +=  '\n\n'.join(text)
     desc = desc.replace('[','').replace(']','')
     embed = discord.Embed(title=header[0], description=desc)
-    embed.set_thumbnail(url=imgurl)
+    embed.set_thumbnail(url=image)
     await ctx.bot.send_message(ctx.message.channel, embed=embed)
+    alternatives = [n['name'] for n in matches[1:]]
+    if alternatives:
+        await ctx.bot.send_message(ctx.message.channel, 'Did you mean: {}?'.format(', '.join(alternatives)))
