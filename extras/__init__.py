a488a832a0a7c190d6b41363bbebcc5fcc54f88f
MT <myat@aung.uk>
2018-01-07 14:24:32 +0000

    basic xkcd API calls
    string conversion for xkcd
    
    committing bugfix attempts because local instance not working
    
    I'm an idiot
    
    set url
    
    keyword whitespace
    
    added footer
    
    isdigit check for callstring
    
    img debug
    
    comic var?
    
    testing messages
    
    I think I got it
    
    change thumbnail to image
    
    whatIf working
    
    SYNTAX ERRORRRRR
    
    more syntax errors
    
    more fixes
    
    removed invalid var
    
    fixed comic subscriptable error
    
    Formatting
    
    more string formatting

--- extras/__init__.py
+++ extras/__init__.py
@@ -1,3 +1,5 @@
 from extras.inspirobot import inspire
 from extras.cointoss import flipcoin
 from extras.stupid import deal
+from extras.xkcd import getxkcd
+from extras.xkcd import whatIf
---
+++ extras/xkcd.py
@@ -0,0 +1,56 @@
+import xkcd
+import asyncio
+import aiohttp
+import discord
+from discord.ext import commands
+
+comic = None
+whatIf = None
+
+print(xkcd.getRandomWhatIf())
+
+@commands.command(name='xkcd', pass_context=True)
+async def getxkcd(ctx, call='random'):
+    """Functions that call to xkcd comics API"""
+    if call == 'random':
+        comic = xkcd.getRandomComic()
+    elif call == 'latest':
+        comic = xkcd.getLatestComic()
+    elif call.isdigit():
+        comic = xkcd.getComic(call)
+    else: 
+        await ctx.bot.send_message(\
+        ctx.message.channel,\
+        'Hmm... I can\'t find a comic with that parameter.')
+    ctitle = comic.getTitle()
+    catext = comic.getAltText()
+    curl = comic.getImageLink()
+    cimgn = comic.getImageName()
+    cnumber = str(comic)[38:]
+    # Embed and send message
+    embed = discord.Embed(title=ctitle, description=catext)
+    embed.set_image(url=curl)
+    embed.set_footer(text='xkcd issue {}: {}'.format(cnumber, cimgn))
+    await ctx.bot.send_message(ctx.message.channel, embed=embed)
+
+@commands.command(pass_context=True)
+async def whatIf(ctx, call='random'):
+    """Functions that call to xkcd whatif API, currently incomplete"""
+    if call == 'random':
+        whif = xkcd.getRandomWhatIf()
+    elif call == 'latest':
+        whif = xkcd.getLatestWhatIf()
+    elif call.isdigit():
+        whif = xkcd.getWhatIf(call)
+    else: 
+        await ctx.bot.send_message(\
+        ctx.message.channel,\
+        'Hmm... I can\'t find a WhatIf with that parameter.')
+    wtitle = whif.getTitle()
+    wurl = whif.getLink()
+    wnumber = whif.getNumber()
+    # Embed and send message
+    embed = discord.Embed(title=wtitle)
+    embed.set_image(url=wurl)
+    embed.set_footer(text='WhatIf issue {}'.format(wnumber))
+    await ctx.bot.send_message(ctx.message.channel, embed=embed)
