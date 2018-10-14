e02f4c9dd321043a60a78b90bbc97a749b26bfa9
Hitsugata <zakie95hinds@gmail.com>
2018-10-14 19:05:23 -0400

    debug

--- Voice/Voice.py
+++ Voice/Voice.py
@@ -6,10 +6,10 @@ class Voice:
     @commands.command(pass_context=True)
     async def jointest(ctx):
         if ctx.message.author.voice is None:
-            await ctx.bot.send_message(ctx.message.channel, 'You ain\'t there. Can\'t connect')
+        #    await ctx.bot.send_message(ctx.message.channel, 'You ain\'t there. Can\'t connect')
             return
         await client.join_voice_channel(ctx.message.author.voice.channel)
-        await ctx.bot.send_message(ctx.message.channel, "Party times boys!")
+        #await ctx.bot.send_message(ctx.message.channel, "Party times boys!")
         return
 
     @commands.command(pass_context=True)
