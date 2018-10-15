8866a4a4ea5a5ce5652aeb8f560138f37379f8b7
Hitsugata <zakie95hinds@gmail.com>
2018-10-15 18:23:16 -0400

    Start of queue system

--- Voice/Queue.py
+++ Voice/Queue.py
@@ -18,10 +18,34 @@ class Queue:
         await Queue.Voice.disconnect(ctx)
 
     def _addqueue(self,yturl):
-        Queue.QueueURL.append(yturl)
+        Queue.QueueURL.insert(0,yturl)
         return
+
+    def _removequeue(self):
+        Queue.QueueURL.pop(0)
+        return
+    @commands.command(pass_context=True)
+    async def clear(self,ctx,url):
+        Queue.QueueURL.clear()
 
     @commands.command(pass_context=True)
     async def play(self,ctx,url):
+        if Queue.Voice.voiceclient is None:
             self._addqueue(url)
             await Queue.Voice.play(ctx,Queue.QueueURL[0])
+            self._removequeue()
+            return
+        else:
+            self._addqueue()
+            return
+
+    @commands.command(pass_context=True)
+    async def next(self,ctx,url):
+        if Queue.Voice.voiceclient is None:
+            await ctx.bot.send_message(ctx.message.channel, 'Bruh, I\'m not even in a channel. :thonking:')
+        if Queue.Voice.player is None:
+            await Queue.Voice.play(ctx,Queue.QueueURL[0])
+        else:
+            await Queue.Voice.stop(ctx)
+            await Queue.Voice.play(ctx,Queue.QueueURL[0])
+            await ctx.bot.send_message(ctx.message.channel, 'Here we go skipping again!')
--- Voice/Voice.py
+++ Voice/Voice.py
@@ -1,11 +1,10 @@
 import discord
 import youtube_dl
 from discord.ext import commands
-
+#you're not allowed here! Begone!
 class Voice:
     voiceclient=None
     player=None
-    #@commands.command(pass_context=True)
     async def join(self,ctx):
         if ctx.message.author.voice is None:
             await ctx.bot.send_message(ctx.message.channel, 'You ain\'t there. Can\'t connect')
@@ -29,10 +28,9 @@ class Voice:
     async def playtest(self,ctx):
         if Voice.voiceclient is None:
             await ctx.bot.send_message(ctx.message.channel, "I'm not connected to channel, attempting to join")
-            #will fix later.
             await self.join(ctx)
-            #await ctx.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
-            #Voice.voiceclient = ctx.bot.voice_client_in(ctx.message.server)
+            Voice.player = Voice.voiceclient.create_ffmpeg_player('victory.mp3')
+            Voice.player.start()
         else:
             await ctx.bot.send_message(ctx.message.channel, "Playing test sound")
         if Voice.player is None:
@@ -43,10 +41,27 @@ class Voice:
             return
         return
 
-
     async def play(self,ctx,url):
         if Voice.voiceclient is None:
             await ctx.bot.send_message(ctx.message.channel, "Let me join first.")
             await self.join(ctx)
         Voice.player = await Voice.voiceclient.create_ytdl_player(url)
         Voice.player.start()
+
+    async def pause(self,ctx):
+        if Voice.voiceclient is None:
+            await ctx.bot.send_message(ctx.message.channel, "Pause? When I'm not there? ....Really?")
+        if Voice.player is None or not Voice.player.is_playing():
+            await ctx.bot.send_message(ctx.message.channel, "Not even playing anything :sus:")
+        else:
+            await Voice.player.pause()
+            await ctx.bot.send_message(ctx.message.channel, "Playback paused.")
+
+    async def stop(self,ctx):
+        if Voice.voiceclient is None:
+            await ctx.bot.send_message(ctx.message.channel, "Can't stop things if I'm not there")
+        if Voice.player is None or not Voice.player.is_playing():
+            await ctx.bot.send_message(ctx.message.channel, "Not even playing anything :sus:")
+        else:
+            await Voice.player.stop()
+            await ctx.bot.send_message(ctx.message.channel, "Stopping!")
