5132d53d027596833f0c6d0027daa39b074c7095
Hitsugata <zakie95hinds@gmail.com>
2018-10-14 22:58:04 -0400

    trying out something

--- Voice/Voice.py
+++ Voice/Voice.py
@@ -25,9 +25,8 @@ class Voice:
         if voiceclient is None:
             await ctx.bot.send_message(ctx.message.channel, "I'm not connected to channel, attempting to join")
             #will replace this next section. I know...I know...
-            #await ctx.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
-            #voiceclient = ctx.bot.voice_client_in(ctx.message.server)
-            self.join(self,ctx)
+            await ctx.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
+            voiceclient = ctx.bot.voice_client_in(ctx.message.server)
         else:
             await ctx.bot.send_message(ctx.message.channel, "Playing test sound")
         player = voiceclient.create_ffmpeg_player('victory.mp3')
