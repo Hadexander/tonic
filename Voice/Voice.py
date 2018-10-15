55d0d4e7993b6f03b894747119ee27f063b9ce81
Hitsugata <zakie95hinds@gmail.com>
2018-10-15 01:00:41 -0400

    Experimenting

--- Voice/Voice.py
+++ Voice/Voice.py
@@ -15,19 +15,17 @@ class Voice:
 
     @commands.command(pass_context=True)
     async def disconnect(self,ctx):
-        #voiceclient = ctx.bot.voice_client_in(ctx.message.server)
         await ctx.bot.send_message(ctx.message.channel, "Crunk time over")
         await Voice.voiceclient.disconnect()
+        Voice.voiceclient=None
         return
 
     @commands.command(pass_context=True)
     async def playtest(self,ctx):
         voiceclient = ctx.bot.voice_client_in(ctx.message.server)
-        if voiceclient is None:
+        if Voice.voiceclient is None:
             await ctx.bot.send_message(ctx.message.channel, "I'm not connected to channel, attempting to join")
-            #will replace this next section. I know...I know...
-            await ctx.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
-            voiceclient = ctx.bot.voice_client_in(ctx.message.server)
+            Voice.join(self,ctx)
         else:
             await ctx.bot.send_message(ctx.message.channel, "Playing test sound")
         player = voiceclient.create_ffmpeg_player('victory.mp3')
