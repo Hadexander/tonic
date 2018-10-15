e44060a6ebade14a71327c8a86e67ea769d7c92c
Hitsugata <zakie95hinds@gmail.com>
2018-10-15 13:33:43 -0400

    Start of queue system

--- core.py
+++ core.py
@@ -3,11 +3,9 @@ import util
 import github
 import poe
 import Voice
-from Voice.Voice import Queue
 def setup(bot):
     util.setup(bot)
     poe.setup(bot)
     github.setup(bot)
     extras.setup(bot)
     Voice.setup(bot)
-    Queue.setup(bot)
