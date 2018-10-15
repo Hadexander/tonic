58bcda6061e313edd62b7ca43bd0c0e97bd83e8b
Hitsugata <zakie95hinds@gmail.com>
2018-10-15 13:47:03 -0400

    Start of queue system

--- Voice/Queue.py
+++ Voice/Queue.py
@@ -14,5 +14,4 @@ class Queue:
 
     @commands.command(pass_context=True)
     async def disconnect(self,ctx):
-        Queue.Voice.voiceclient.disconnect(ctx)
-        
+        await Queue.Voice.voiceclient.disconnect(ctx)
