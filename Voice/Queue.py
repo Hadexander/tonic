af5f128d3fdc5d99411be586ee4c6f36e9e1a576
Hitsugata <zakie95hinds@gmail.com>
2018-10-16 03:16:53 -0400

    Start of Queue System Testing

--- Voice/Queue.py
+++ Voice/Queue.py
@@ -6,6 +6,7 @@ class Queue:
     QueueURL=[]
     @commands.command(pass_context=True)
     async def join(self,ctx):
+        """Bot joins current user's channel"""
         if Voice.voiceclient is None:
             await Queue.Voice.join(ctx)
             return
@@ -15,24 +16,29 @@ class Queue:
 
     @commands.command(pass_context=True)
     async def disconnect(self,ctx):
+        """Disconnects from current channel"""
         await Queue.Voice.disconnect(ctx)
         return
 
     def _addqueue(self,yturl):
+        """Adds url to a queue list in case a song is already playing"""
         Queue.QueueURL.append(yturl)
         return
 
     def _removequeue(self):
+        """Removes first item in queue list"""
         Queue.QueueURL.pop(0)
         return
     @commands.command(pass_context=True)
     async def clear(self,ctx):
+        """Clears entire queue. Becareful!"""
         Queue.QueueURL.clear()
-        await ctx.bot.send_message(ctx.message.channel, 'All music empty. Like this bottle of Gin.')
+        await ctx.bot.send_message(ctx.message.channel, 'Music queue empty. Like this bottle of Gin.')
         return
 
     @commands.command(pass_context=True)
     async def play(self,ctx,url):
+        """Plays youtube links. IE 'https://www.youtube.com/watch?v=mPMC3GYpBHg' """
         if Queue.Voice.voiceclient is None:
             self._addqueue(url)
             await Queue.Voice.play(ctx,Queue.QueueURL[0])
@@ -44,6 +50,7 @@ class Queue:
 
     @commands.command(pass_context=True)
     async def next(self,ctx):
+        """Plays song next in queue."""
         if Queue.Voice.voiceclient is None:
             await ctx.bot.send_message(ctx.message.channel, 'Bruh, I\'m not even in a channel. :thonking:')
             return
@@ -57,7 +64,25 @@ class Queue:
             return
 
     @commands.command(pass_context=True)
+    async def pause(self,ctx):
+        """Pauses song"""
+        await Queue.Voice.pause(ctx)
+        return
+
+    @commands.command(pass_context=True)
+    async def stop(self,ctx):
+        """Stops playback"""
+        await Queue.Voice.stop(ctx)
+        return
+
+    @commands.command(pass_context=True)
+    async def resume(self,ctx):
+        """Resumes playback"""
+        await Queue.Voice.resume(ctx)
+        return
+
     async def setvolume(self,ctx, vol):
+        """Sets volume between 0 and 200."""
         vol = int(vol)
         await ctx.bot.send_message(ctx.message.channel, 'volume input = '+ vol)
         await Queue.Voice.setvolume(ctx,vol)
--- Voice/Voice.py
+++ Voice/Voice.py
@@ -7,20 +7,22 @@ class Voice:
     player=None
     volume=0.5
     async def join(self,ctx):
+        """Bot joins current user's channel"""
         if ctx.message.author.voice is None:
             await ctx.bot.send_message(ctx.message.channel, 'You ain\'t there. Can\'t connect')
             return
         await ctx.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
         Voice.voiceclient = ctx.bot.voice_client_in(ctx.message.server)
-        await ctx.bot.send_message(ctx.message.channel, "Party times boys!")
+        await ctx.bot.send_message(ctx.message.channel, "Party time boys!")
         return
 
     async def disconnect(self,ctx):
+        """Disconnects from current channel"""
         if Voice.voiceclient is None:
             await ctx.bot.send_message(ctx.message.channel, "I'm not even in the channel...? :thinking:")
             return
         else:
-            await ctx.bot.send_message(ctx.message.channel, "Crunk time over")
+            await ctx.bot.send_message(ctx.message.channel, "Crunk time over. Wu-tang out!")
             await Voice.voiceclient.disconnect()
             Voice.voiceclient=None
         return
@@ -45,6 +47,7 @@ class Voice:
         return
 
     async def play(self,ctx,url):
+        """Plays youtube links. IE 'https://www.youtube.com/watch?v=mPMC3GYpBHg' """
         if Voice.voiceclient is None:
             await ctx.bot.send_message(ctx.message.channel, "Let me join first.")
             await self.join(ctx)
@@ -54,18 +57,33 @@ class Voice:
         return
 
     async def pause(self,ctx):
+        """Pauses current song."""
         if Voice.voiceclient is None:
             await ctx.bot.send_message(ctx.message.channel, "Pause? When I'm not there? ....Really?")
             return
         if Voice.player is None or not Voice.player.is_playing():
-            return
             await ctx.bot.send_message(ctx.message.channel, "Not even playing anything :sus:")
+            return
         else:
             await Voice.player.pause()
             await ctx.bot.send_message(ctx.message.channel, "Playback paused.")
             return
 
+    async def resume(self,ctx):
+        """Resumes current song."""
+        if Voice.voiceclient is None:
+            await ctx.bot.send_message(ctx.message.channel, "Resume? When I'm not there? ....Really?")
+            return
+        if Voice.player is None or Voice.player.is_playing():
+            await ctx.bot.send_message(ctx.message.channel, "I'm not playing anything or I'm already playing something.")
+            return
+        else:
+            await Voice.player.resume()
+            await ctx.bot.send_message(ctx.message.channel, "Playback paused.")
+            return
+
     async def stop(self,ctx):
+        """Stops current song."""
         if Voice.voiceclient is None:
             await ctx.bot.send_message(ctx.message.channel, "Can't stop things if I'm not there")
             return
@@ -78,7 +96,8 @@ class Voice:
             return
 
     async def setvolume(self,ctx,vol):
-        if vol < 200 or vol > 0:
+        """Sets global volume of stream."""
+        if vol > 200 or vol < 0:
             await ctx.bot.send_message(ctx.message.channel, "Please set volume higher than 0 and lower than 200")
             return
         else:
