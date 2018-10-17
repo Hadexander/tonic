878a5f9687f5ee6482b33fbb4ce16857e1ffe8b0
SchrodingersKitty <radium213@gmail.com>
2018-10-17 07:01:57 +0300

    Simple polling command.

---
+++ extras/poll.py
@@ -0,0 +1,21 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+class Polling:
+    @commands.command(pass_context=True)
+    async def poll(self, ctx, *args):
+        """Starts a voting poll on the channel. Voting is done with reactions. Default duration is 60 seconds."""
+        text = '@here ' + ' '.join(args)
+        msg = await ctx.bot.send_message(ctx.message.channel, text)
+        options = ['👍', '👎']
+        for r in options:
+            await ctx.bot.add_reaction(r)
+        await asyncio.sleep(60)
+        results = {}
+        for r in msg.reactions:
+            if str(r) in options:
+                results[str(r)] = r.count - (1 if r.me else 0)
+        m = max(results.values())
+        winners = [k for k,v in results.items() if v == m]
+        await ctx.bot.edit_message(msg, "{} [result:{}]".format(text, ''.join(winners)))
