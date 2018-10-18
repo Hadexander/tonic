a95044a3c35e5c6e46f46067c1fa4a07a9c4bda0
SchrodingersKitty <radium213@gmail.com>
2018-10-18 10:33:16 +0300

    experiment 3

--- Voice/Voice.py
+++ Voice/Voice.py
@@ -56,7 +56,7 @@ class Voice:
             await ctx.bot.send_message(ctx.message.channel, "Let me join first.")
             await self.join(ctx)
         try:
-            ytdl_opts = {'format': 'bestaudio/best/webm[abr>0]'}
+            ytdl_opts = {'format': 'webm[abr>0]/bestaudio/best'}
             Voice.player = await Voice.voiceclient.create_ytdl_player(url, ytdl_opts)
         except:
                 #raise BadArgument()
