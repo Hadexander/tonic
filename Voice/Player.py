f29da9a97d568063d5ef3a58877994ea0056998f
Hitsugata <zakie95hinds@gmail.com>
2018-10-21 00:40:42 -0400

    debug

--- Voice/Player.py
+++ Voice/Player.py
@@ -122,7 +122,6 @@ class Player:
             validation_play_check = await self._play(ctx,self.QueueURL[0])
             self._removequeue()
             return
-'''
         if self.players[servername].is_playing():
             await ctx.bot.send_message(ctx.message.channel, "I'm already playing something but I'll add it to the queue!")
             self._addqueue(url)
@@ -134,7 +133,7 @@ class Player:
         self._removequeue()
         return
 
-
+'''
     @commands.command(pass_context=True)
     async def next(self,ctx):
         """Plays song next in queue."""
