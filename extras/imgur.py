0d26fd7f08ad0f4681be125e3130b3c22d6e16fc
SchrodingersKitty <radium213@gmail.com>
2018-11-11 01:43:02 +0200

    imgur module fully fixed

--- extras/imgur.py
+++ extras/imgur.py
@@ -3,33 +3,40 @@ import asyncio
 import aiohttp
 from storage import settings
 
-async def _update_access_token(settings):
-    """Generates or updates an imgur API access token using imgur account data provided in settings."""
+time_format = "%Y-%m-%dT%H:%M:%S"
+
+async def _update_access_token(config):
+    """Generates or updates an imgur API access token using imgur account data provided in config."""
     now = datetime.now()
-    if(settings.imgur_expiration < now):
+    exp_str = config.get('expiration')
+    try:
+        exp = datetime.strptime(exp_str, time_format)
+    except ValueError:
+        exp = None
+    if not exp or exp < now:
         async with aiohttp.ClientSession() as session:
             body = {
-                'refresh_token':settings.imgur_refresh,
-                'client_id':settings.imgur_id,
-                'client_secret':settings.imgur_secret,
+                'refresh_token':config.get('refresh'),
+                'client_id':config.get('id'),
+                'client_secret':config.get('secret'),
                 'grant_type':'refresh_token'
             }
             async with session.post('https://api.imgur.com/oauth2/token', data=body) as response:
                 if(response.status == 200):
                     resp = await response.json()
-                    settings.imgur_access = resp.get('access_token')
-                    exp = resp.get('expires_in', 0)
-                    settings.imgur_expiration = now + timedelta(seconds=exp)
-                    settings.save()
+                    config['access'] = resp.get('access_token')
+                    exp_seconds = resp.get('expires_in', 0)
+                    exp = now + timedelta(seconds=exp_seconds)
+                    config['expiration'] = datetime.strftime(exp, time_format)
+                    settings.save('Imgur', config)
 
 async def image_upload(url):
     """Uploads an image to imgur by url. Returns a link and image id on success, error message on failure."""
     config = settings.load('Imgur')
-    #disabled temporarily
-    #await _update_access_token(settings)
+    await _update_access_token(settings)
     async with aiohttp.ClientSession() as session:
         headers = {
-            'Authorization':'Client-ID '+config.get('id'),
+            #'Authorization':'Client-ID '+config.get('id'),
             'Authorization':'Bearer '+config.get('access')
         }
         body = {
@@ -46,7 +53,7 @@ async def image_upload(url):
 async def image_delete(id):
     """Deletes an image from imgur."""
     config = settings.load('Imgur')
-    #await _update_access_token(settings)
+    await _update_access_token(settings)
     async with aiohttp.ClientSession() as session:
         headers = {
             'Authorization':'Bearer '+config.get('access')
