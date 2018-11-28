06c2442e1159ab5d1c2995d0f8b3c242ee6e604a
Hitsugata <zakie95hinds@gmail.com>
2018-11-28 13:32:17 -0500

    Genres added

--- steam_tonic/steam_tonic.py
+++ steam_tonic/steam_tonic.py
@@ -12,6 +12,8 @@ class Steam_Tonic:
         response = json.loads(requests.get('https://store.steampowered.com/api/appdetails?appids=%s' %appid).content)
         #Checks if there was a valid response
         metacritic_score = "None"
+        genres = ""
+        g_count = 0
         if response is None or 'data' not in response[appid]:
             await ctx.bot.send_message(ctx.message.channel, "Game not found")
             return
@@ -34,14 +36,19 @@ class Steam_Tonic:
             if 'metacritic' in response[appid]['data']:
                 metacritic_score = response[appid]['data']['metacritic']['score']
                 metacritic_url = response[appid]['data']['metacritic']['url']
+        #Build Genre table
+        for genre in response[appid]['data']['genres']
+            genres+="{} ".format(genre[g_count]['description'])
+            g_count+=1
         #Builds data from response
         g_name = response[appid]['data']['name']
-        message = """ Game: {} \n Developer: {} \n Publisher: {} \n Description: {} \n {} \n Price(EUR): {}
+        message = """ Game: {} \n Developer: {} \n Publisher: {} \n Description: {} \n Genres: {} \n {} \n Price(EUR): {}
         Metacritic Score: {} """.format(
         g_name,
         response[appid]['data']['developers'][0],
         response[appid]['data']['publishers'][0],
         response[appid]['data']['short_description'],
+        genres,
         date,
         price,
         metacritic_score)
