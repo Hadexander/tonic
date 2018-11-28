37bffa9a9d57e297ca041f09f9b243a1c6c3a34a
Hitsugata <zakie95hinds@gmail.com>
2018-11-28 12:18:00 -0500

    Fixed meta score

--- steam_tonic/steam_tonic.py
+++ steam_tonic/steam_tonic.py
@@ -11,6 +11,7 @@ class Steam_Tonic:
         #Calls api for appid (will later be provided by an internal DB)
         response = json.loads(requests.get('https://store.steampowered.com/api/appdetails?appids=%s' %appid).content)
         #Checks if there was a valid response
+        metacritic_score = "None"
         if response is None or 'data' not in response[appid]:
             await ctx.bot.send_message(ctx.message.channel, "Game not found")
             return
@@ -36,7 +37,7 @@ class Steam_Tonic:
         #Builds data from response
         g_name = response[appid]['data']['name']
         message = """ Game: {} \n Developer: {} \n Publisher: {} \n Description: {} \n {} \n Price(EUR): {}
-        \n Metacritic Score: {} """.format(
+        Metacritic Score: {} """.format(
         g_name,
         response[appid]['data']['developers'][0],
         response[appid]['data']['publishers'][0],
