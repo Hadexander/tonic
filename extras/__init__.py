8a34f2a9f7cde4af01dfaaf48c0e503791b7a4cf
Hitsugata <zakie95hinds@gmail.com>
2018-12-10 13:23:44 -0500

    we don't import things that doesnt exist...

--- extras/__init__.py
+++ extras/__init__.py
@@ -2,7 +2,6 @@ from extras.xkcd import Xkcd
 from extras.fun import Fun
 from extras.emoji import Emoji
 from extras.poll import Polling
-from extras.bartender import Bartender
 def setup(bot):
     bot.add_cog(Xkcd())
     bot.add_cog(Fun())
