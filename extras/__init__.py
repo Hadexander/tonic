c039afaa7b85e03667426459ab93f43d89e70264
Hitsugata <zakie95hinds@gmail.com>
2018-12-10 11:19:24 -0500

    forgot to import bartender...

--- extras/__init__.py
+++ extras/__init__.py
@@ -2,9 +2,10 @@ from extras.xkcd import Xkcd
 from extras.fun import Fun
 from extras.emoji import Emoji
 from extras.poll import Polling
-
+from extras.bartender import Bartender
 def setup(bot):
     bot.add_cog(Xkcd())
     bot.add_cog(Fun())
     bot.add_cog(Emoji(bot))
     bot.add_cog(Polling())
+    bot.add_cog(Bartender())
