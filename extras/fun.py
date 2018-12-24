63e0676921e6a8d7061a2faec7a36d3237a00b9d
Hitsugata <zakie95hinds@gmail.com>
2018-12-24 14:56:04 -0500

    MAL complete.

--- extras/fun.py
+++ extras/fun.py
@@ -111,7 +111,6 @@ class Fun:
     async def mal(self,ctx):
         """Quickly search something on MyAnimeList.....weebs.."""
         anime = ctx.message.content[4:]
-        print(anime)
         response = requests.get("https://api.jikan.moe/v3/search/anime?q={}".format(anime))
         if response.status_code != 200:
             await ctx.bot.send_message(ctx.message.channel, "Anime not found.")
