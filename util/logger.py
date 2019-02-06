1bd69b3579e2d567cf0356bef0fd22efb035449e
SchrodingersKitty <radium213@gmail.com>
2019-02-07 01:51:33 +0200

    Logging forPlayer module.

--- util/logger.py
+++ util/logger.py
@@ -10,8 +10,19 @@ class DiscordLoggingHandler(logging.Handler):
     def emit(self, record):
         record_fmt = self.format(record)
         coro = self.bot.send_message(self.channel, record_fmt)
-        asyncio.run_coroutine_threadsafe(coro, self.bot.loop)
+        future = asyncio.run_coroutine_threadsafe(coro, self.bot.loop)
+        try:
+            future.result()
+        except:
+            return
 
 class DiscordLoggingFormatter(logging.Formatter):
     def format(self, record):
         return f"``{record.levelname}:{record.name}``\n```{record.msg}```"
+
+class LoggingErrorWriter:
+    def __init__(self, logger):
+        self.logger = logger
+    
+    def write(self, message):
+        self.logger.error(message)
